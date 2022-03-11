cac0c6e2b6ddf8538ecb5db2726ea4ca39d63393
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-03-11 20:47:42 -0300

    display corrections

--- calc.js
+++
@@ -1,8 +0,0 @@
-// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const displayNumber = document.getElementById("displayNumber");
-// displayNumber.innerHTML = numbers.map((item) => {
-//   const divNumbers = document.createElement("div");
-//   divNumbers.innerHTML = item;
-//   displayNumber.appendChild(divNumbers);
-//   //   return "text";
-// });
