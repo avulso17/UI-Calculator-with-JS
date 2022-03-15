03d1ee0dafd5913f031bdb39cec63127f9cef368
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-03-15 20:40:17 -0300

    footer add

--- calc.js
+++ calc.js
@@ -1,10 +1,21 @@
+// link function
+function link(path) {
+  window.location.href(path);
+}
+
 const display = document.getElementById("mainDisplay");
 const numbers = document.querySelectorAll("[id*=number]");
+const operators = document.querySelectorAll("[id*=Operator]");
 
 const displayAtt = (text) => {
   display.textContent = text;
 };
 
 const insertNumber = (e) => displayAtt(e.target.textContent);
+const insertOperator = (e) => displayAtt(e.target.textContent);
 
 numbers.forEach((number) => number.addEventListener("click", insertNumber));
+
+operators.forEach((operator) =>
+  operator.addEventListener("click", insertOperator)
+);
